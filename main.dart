dbecbcd1e6edfbbbda31c5e2e07339269c1d19d7
Harendra Prajapati <harendraprajapati72@gmail.com>
2023-02-22 15:34:13 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'app.dart';
+import 'model/app_state_model.dart';
+
+void main() {
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      create: (_) => AppStateModel()..loadProducts(),
+      child: const CupertinoStoreApp(),
+    ),
+  );
+}
